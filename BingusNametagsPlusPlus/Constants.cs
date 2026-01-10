df210b2219c821b908f8847ba2aa703e82aa9584
bingus <bendawsdev@gmail.com>
2026-01-10 14:26:23 -0600

    update verison

--- BingusNametagsPlusPlus/Constants.cs
+++ BingusNametagsPlusPlus/Constants.cs
@@ -8,9 +8,9 @@ public static class Constants
 {
 	public const string Name = "BingusNametags++";
 	public const string Guid = "bingus.nametagsplusplus";
-	public const string Version = "1.3.3";
+	public const string Version = "1.3.2";
 
-    public const ReleaseChannel Channel = ReleaseChannel.Beta;
+    public const ReleaseChannel Channel = ReleaseChannel.Stable;
 
     public static string BingusNametagsData =>
         Path.Combine(Application.dataPath[.. Application.dataPath.LastIndexOf("/")], "BingusNametags++");
