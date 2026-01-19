2dd51e88d3adee93a236d828bcdec1804cff85cf
bingus <bendawsdev@gmail.com>
2026-01-18 18:55:41 -0600

    1.3.3

--- BingusNametagsPlusPlus/Attributes/BingusNametagsPlugin.cs
+++ BingusNametagsPlusPlus/Attributes/BingusNametagsPlugin.cs
@@ -5,7 +5,7 @@ namespace BingusNametagsPlusPlus.Attributes
     /// <summary>
     /// BingusNametagsPlugin holds the metadata of a plugin.
     /// </summary>
-    public class BingusNametagsPlugin(string name, string author, string description, float offset = 50f, string[]? unsupported = null) : Attribute
+    public class BingusNametagsPlugin(string name, string author, string description, float offset, string[]? unsupported = null) : Attribute
     {
         /// <summary>
         /// The display name of the nametag. This is used on the Plugins tab.
@@ -30,7 +30,7 @@ namespace BingusNametagsPlusPlus.Attributes
         /// <summary>
         /// Whether BingusNametags++ automatically calculates the offset of this nametag.
         /// </summary>
-        public bool AutomaticOffsetCalculation = offset >= 50f;
+        public bool AutomaticOffsetCalculation = false;
 
         /// <summary>
         /// Any unsupported plugins. These will be turned off when your nametag is enabled.
