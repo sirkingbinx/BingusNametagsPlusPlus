25e45e0fcd679d248e34cdfe804f394297a496ea
bingus <bendawsdev@gmail.com>
2026-01-27 20:30:20 -0600

    changed advanced nametags to be lower than the normal nametag

--- BingusNametagsPlusPlus/Nametags/AdvancedNametag.cs
+++ BingusNametagsPlusPlus/Nametags/AdvancedNametag.cs
@@ -8,10 +8,10 @@ namespace BingusNametagsPlusPlus.Nametags;
 [BingusNametagsPlugin("Advanced", "Bingus", "Extra nametag fields with FPS and speed in m/s.")]
 public class AdvancedNametag : IBaseNametag
 {
-    [BingusNametagsNametag("Advanced", 0.5f)]
+    [BingusNametagsNametag("Advanced", -0.35f)]
     public static void UpdateNametag(PlayerNametag nametag)
     {
-        nametag.Size = ConfigManager.Scale * 0.85f;
+        nametag.Size = 0.85f;
         nametag.AddStyle("color", "#d6d6d6");
         nametag.Text = $"{nametag.Owner.fps} fps / {nametag.Owner.LatestVelocity().magnitude:F1} m/s";
     }
